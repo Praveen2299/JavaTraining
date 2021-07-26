c69ca19035cb2bada5093d970657fd9415233ad3
Praveen2299 <mpraveenmuthu22@gmail.com>
2021-07-26 12:46:11 +0530

    Sample Codes

---
+++ rectangle/Rectangle.java
@@ -0,0 +1,20 @@
+package rectangle;
+
+public class Rectangle {
+	
+	private double length;
+	private double breath;
+	
+	public Rectangle(int i, int j) {
+		// TODO Auto-generated constructor stub
+		this.length = i;
+		this.breath = j;
+		
+	}
+
+
+	
+	//FindSides object = new FindSides(5,6);
+	
+    
+}
